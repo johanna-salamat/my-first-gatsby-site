87037997b195ce4c9eba50778a1962fc13666f89
johanna-salamat <johanna.salamat@gmail.com>
2021-09-05 22:16:05 +1000

    Created reusable layout with children

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,14 @@
 //Step 1: Import React from 'react'
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 //Step 2: Define the component. Must begin with a capital letter
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1> Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
+    <Layout pageTitle="Home Page">
       <p>I am making this by following the Gatsby Tutorial.</p>
-    </main>
-    )
+    </Layout>
+  )
 }
 
 //Step 3: Export the component
